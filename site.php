b25289e4b2788f11528fc048f416ef8d1f27d570
“[firstname <peza361@gmail.com>
2021-02-13 22:51:17 -0500

    Finished portfolio page, stated php

---
+++ site.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $characterName = "Maxine";
+      $characterAge = 35;
+      $phrase = "to be or not to be";
+      $gpa = 3.8;
+      $isMale = false;
+      echo("Hello $characterName");
+      echo strtolower($characterName);
+      echo "<hr>";
+      echo "<p>You are $characterAge years old</p>";
+      echo $characterName[5];
+      echo $characterName[1];
+      echo $characterName[2];
+      echo 5/9;
+      echo "<br>";
+      echo 10%3;
+      echo pow(3,3);
+
+      
+     ?>
+  </body>
+</html>
